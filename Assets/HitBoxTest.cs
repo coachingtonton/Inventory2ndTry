96eb06c2fc5ffba792f5136644f6d1bd21f6ad8d
coachingtonton <coachingtonton@gmail.com>
2026-04-25 18:30:07 -0700

    Learnt how to use melee weapons, saving primitives for progress video

--- Assets/HitBoxTest.cs
+++ Assets/HitBoxTest.cs
@@ -1,40 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// THIS SCRIPT IS ME LEARNING HOW TO PROPERLY PROGRAM MELEE WEAPONS.
+/// </summary>
 public class HitBoxTest : MonoBehaviour
 {
     [SerializeField] Transform attackPoint;
     [SerializeField] LayerMask enemyLayer;
     [SerializeField] Vector2 boxSize;
+    public float windupDelay;
+    public float coolDownTimer = .2F;
+    public bool canAttack;
+    float knockBackForce = 5f;
     Collider2D[] hits;
+    public float directionY;
+    public float directionX;
+
+
+    public void Start()
+    {
+        canAttack = true;
+    }
 
     public void Update()
     {
-        UseHitbox();
+        if (InputManager.Instance.threePressed && canAttack == true)
+        {
+            StartCoroutine(SwingAttack());
+        }
+
     }
 
     public void UseHitbox()
     {
-        if (InputManager.Instance.threePressed)
-        {
-            hits  = Physics2D.OverlapBoxAll(attackPoint.position, boxSize, 0f, enemyLayer);
-        }
+        hits = Physics2D.OverlapBoxAll(attackPoint.position, boxSize, 0f, enemyLayer);
 
         if (hits == null)
         {
             return;
         }
 
-        foreach (Collider2D hit in hits)
-        {
-
-            Debug.Log(hit.gameObject.name);
-        }
-
-
     }
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireCube(attackPoint.position, boxSize);
     }
+
+    public IEnumerator SwingAttack()
+    {
+        canAttack = false;
+
+        HashSet<Collider2D> EnemiesHit = new HashSet<Collider2D>();
+
+        float timer = 0f;
+        float swingDurationTimer = 0.5f;
+        
+
+        yield return new WaitForSeconds(windupDelay);
+        
+        while (timer < swingDurationTimer )
+        { // HITBOX WILL BE PRESENT AS LONMG AS TIMER IS LESS THAN SWING DURATION 
+            
+            UseHitbox();
+            timer += Time.deltaTime;
+
+            foreach (var hits in hits)
+            {
+                if (EnemiesHit.Contains(hits)) continue;
+                ///IF ENEMY IS ALREADY IN HASH SET, SKIP REST OF BLOCK
+
+                Vector2 direction = (hits.transform.position + - attackPoint.position).normalized;
+                direction.y *= directionY;
+                direction.x *= directionX;
+
+
+                EnemiesHit.Add(hits);
+
+                Rigidbody2D rb = hits.GetComponent<Rigidbody2D>();
+                if (rb !=null)
+                {
+                    rb.AddForce(direction * knockBackForce, ForceMode2D.Impulse);
+                }
+
+                Debug.Log(hits.name);
+            }
+            yield return null;
+        }
+        yield return new WaitForSeconds(coolDownTimer);
+        canAttack = true;
+    }
+
 }
--- Assets/Testing.cs
+++ Assets/Testing.cs
@@ -1,15 +1,33 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Testing : MonoBehaviour 
 {
-    public Enemy enemy;
-    public ResourcePool pool;
+    HashSet<int> randomNum = new HashSet<int>();
 
-    public void Start()
+    public void Update()
     {
-        pool = enemy.health;
+        //if (InputManager.Instance.fourPressed)
+        //{
+        //    int number = UnityEngine.Random.Range(1,9);
+
+        //    if (randomNum.Contains(number) == false)
+        //    {
+        //        randomNum.Add(number);
+        //        Debug.Log("added " + number);
+        //    }
+        //    else
+        //        Debug.Log(number + " ALREADY EXISTS");
+        //}
+
+        //if (InputManager.Instance.fivePressed)
+        //{
+        //    foreach (int number in randomNum)
+        //    {
+        //        Debug.Log(number);
+        //    }
+        //}
     }
-    
 }
 
